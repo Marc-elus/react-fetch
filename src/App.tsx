1edea4413b303f8bf006c29b182ad3ef7fb89f16
Marcela <slobodaniuc1994@gmail.com>
2023-11-02 14:12:28 +0000

    new project done

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import{useState, Dispatch, SetStateAction} from'react';
+type GenPic = [{
+  url:string
+}]
+
+function App() {
+  const [data, setData]=useState(null) as [GenPic|null,Dispatch<SetStateAction<null>>]
+  const[userInput, setUserInput]= useState("")
+  const myStyle= {
+    width: "400px"
+  }
+ const generate = ()=>{
+ 
+fetch(`https://main--papaya-cranachan-0dd5ad.netlify.app/.netlify/functions/create-pic?prompt=${userInput}`)
+.then((response)=>{
+return response.json()
+})
+.then(json=>{
+  console.log (json)
+  setData (json)
+})
+
+ }
+  return (
+   <div>
+    <label>What kind of picture do you want</label>
+    <input onChange={(e)=>setUserInput(e.target.value)} type="text"></input>
+    <button onClick={generate}>Generate picture</button>
+    {(!data)?
+    <h2>here will come a piture</h2>:
+    <img style={myStyle} src={data[0].url}></img>
+  }
+    <button>click no</button>
+    </div>
+  );
+}
+
+export default App;
